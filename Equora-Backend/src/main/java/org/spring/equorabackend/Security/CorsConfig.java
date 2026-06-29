ad850168eb9930d0bb3284e0cac0c966334f374e
Ayushi Singh <1032241223@tcetmumbai.in>
2026-06-29 20:55:48 +0530

    Update CORS configuration for allowed origins and methods

--- Equora-Backend/src/main/java/org/spring/equorabackend/Security/CorsConfig.java
+++ Equora-Backend/src/main/java/org/spring/equorabackend/Security/CorsConfig.java
@@ -6,14 +6,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins(
-                        "http://localhost:5173"
-//                        "https://your-app.vercel.app"
+                        "http://localhost:5173",
+                        "https://equora-split-expense.vercel.app"
                 )
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
